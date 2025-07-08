274c0f5f8a19b895badf1c8e19892a34479c7db6
Firmin.sun <fmsunyh@gmail.com>
2025-07-08 10:53:43 +0800

    1

--- backend/app/tasks/video/tasks.py
+++ backend/app/tasks/video/tasks.py
@@ -12,16 +12,19 @@ def process_video(task: dict):
     features = task.get("features")
     if features:
         print("Processing features:", features)
+
         if "mirrorflip" in features:
             cmd += ["-vf", "hflip"]
+
         if "md5" in features:
             cmd += ["-metadata", "comment=md5_patch"]
+
         if "breakoffbothends" in features:
             duration = get_video_duration(input_path)
             start = 3
             end = max(duration - 3, start + 0.1)
             cmd += ["-ss", str(start), "-to", str(end)]
-            
+
         if "randomrotation" in features:
             angle = random.choice([2, -2])
             zoom_ratio = 1.1
@@ -38,6 +41,9 @@ def process_video(task: dict):
     effects = task.get("effects")
     if effects:
         print("Processing effects:", effects)
+
+        # if "mirrorflip" in features:
+        #     cmd += ["-vf", "hflip"]
 
     cmd += [output_path]
     subprocess.run(cmd, check=True)
